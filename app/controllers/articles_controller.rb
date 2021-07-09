b9b28458b49a4fab3766fe07e3542e865ec3e48a
Jordan Newberry <jordan.newberry@gmail.com>
2021-07-09 13:49:04 -0500

    blog finished with simple authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "JordanNewberry", password: "ILov3C4ll3y!",
+except: [:index, :show]
+
     def index
         @articles = Article.all
     end
